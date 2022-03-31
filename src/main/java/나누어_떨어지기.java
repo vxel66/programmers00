ea259f5a798e0ab237f540d3d893d179db9ca3c6
minwook <vxel7408@gmail.com>
2022-03-31 22:36:14 +0900

    1

--- src/main/java/나누어_떨어지기.java
+++ src/main/java/나누어_떨어지기.java
@@ -11,18 +11,19 @@ public class 나누어_떨어지기 {
 
     public int[] solution(int[] arr, int divisor) {
         int length = 0;
+        // 배열 크기
         for(int i = 0; i < arr.length; i++){
             if(arr[i] % divisor == 0) length++;
         }
-
+        // 배열 선언
         int[] answer = new int[length];
-
+        // 해당 값으 없을시
         if(length==0){
             answer = new int[1];
             answer[0]=-1;
             return answer;
         }
-
+        // 값 삽입
         for(int i =0; i<arr.length; i++){
             if(arr[i]%divisor==0){
                 for(int j =0; j<answer.length;j++){
