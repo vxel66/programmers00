658a66eed494c12bcad4d8b9c284d8a91f232905
minwook <vxel7408@gmail.com>
2022-04-03 13:06:25 +0900

    1

--- src/main/java/나누어_떨어지기.java
+++ src/main/java/나누어_떨어지기.java
@@ -23,6 +23,7 @@ public class 나누어_떨어지기 {
             answer[0]=-1;
             return answer;
         }
+
         // 값 삽입
         for(int i =0; i<arr.length; i++){
             if(arr[i]%divisor==0){
