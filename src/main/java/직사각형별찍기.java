90d790ded8d423f4803329fa4f6e67003cfb2e2b
minwook <vxel7408@gmail.com>
2022-04-07 08:52:18 +0900

    1

---
+++ src/main/java/직사각형별찍기.java
@@ -0,0 +1,14 @@
+public class 직사각형별찍기 {
+
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        for(int x = 0; x < b ; x++){
+            for(int i = 0 ; i < a; i++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
